10090a7198e474b4fe589569ceb6e125021e50d3
Carlos Santos <karscx@gmail.com>
2024-09-30 10:42:49 -0300

    Clean css

--- src/layouts/MainLayout/components/HeaderLayout/HeaderLayout.tsx
+++ src/layouts/MainLayout/components/HeaderLayout/HeaderLayout.tsx
@@ -1,33 +1,16 @@
 import { FC } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from 'assets/react.svg'
-import { ArrowLeftIcon } from 'components/Icons'
 import { PATHS_HOME } from 'Home/routes'
 import styles from './headerLayout.module.scss'
 
 const HeaderLayout: FC = () => {
-  const navigate = useNavigate()
-
   return (
-    <>
-      <header className={styles.header}>
-        <Link className={styles.headerLogo} to={PATHS_HOME.HOME}>
-          <img src={logo} />
-        </Link>
-      </header>
-      <div className={styles.headerNav}>
-        <Link
-          to={'..'}
-          onClick={(e) => {
-            e.preventDefault()
-            navigate(-1)
-          }}
-          className={styles.backButton}
-        >
-          <ArrowLeftIcon />
-        </Link>
-      </div>
-    </>
+    <header className={styles.header}>
+      <Link className={styles.headerLogo} to={PATHS_HOME.HOME}>
+        <img src={logo} />
+      </Link>
+    </header>
   )
 }
 
